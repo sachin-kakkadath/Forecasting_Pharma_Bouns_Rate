d384e732a54084f54edba4acab9fe70e54a6856b
Sachin Kakkadath <59023142+sachin-kakkadath@users.noreply.github.com>
2023-09-28 17:11:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ app = FastAPI()
 
 model1 = pickle.load(open("drug_1.pkl","rb"))
 model2 = pickle.load(open("drug_2.pkl","rb"))
-#model3 = pickle.load(open("drug_3.pkl","rb"))
-#model4 = pickle.load(open("drug_4.pkl","rb"))
+model3 = pickle.load(open("drug_3.pkl","rb"))
+model4 = pickle.load(open("drug_4.pkl","rb"))
 #model5 = pickle.load(open("drug_5.pkl","rb"))
 
 @app.get('/')
@@ -33,10 +33,14 @@ def get_pharama_bounce_rate(req : pharmainput):
     quant = req.quantity
     drn = req.drugname
 
-    if drn == 'SODIUM CHLORIDE 0.9%':
+    if drn == 'MULTIPLE ELECTROLYTES 500ML IVF':
         model = model1
-    elif drn == 'SODIUM CHLORIDE IVF 100ML':
+    elif drn == 'SODIUM CHLORIDE 0.9%':
         model = model2
+    elif drn == 'SODIUM CHLORIDE IVF 100ML':
+        model = model3
+    elif drn == 'PARACETAMOL 1GM IV INJ':
+        model = model4
     else :
         return{"error" : "drug not found" }
     
